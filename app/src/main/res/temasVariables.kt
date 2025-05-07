6b35dd1e81da07217d2cd2c554259814bc9bc945
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-05-06 18:37:26 -0600

    Primer commit desde mi proyecto Android

---
+++ app/src/main/res/temasVariables.kt
@@ -0,0 +1,68 @@
+fun main(){
+    // Para definir variables, se realiza lo siguiente:
+    // Variables mutables 'var'
+    // val a = 4
+    // cal c = 10
+    // cal b = 8
+
+    // println(a)
+    // println("El valor de b es $b")
+    // println("El valor de c es: ${c+2}")
+
+    // c+=5
+    // c-=7
+    // c*=4
+    // c/=2
+
+    // var num1:int = 23
+    // var num2:int = 12
+
+    // num2=6
+
+    // var nombre:String="Isaac"
+    // car caracter:char="a"
+
+    // var num4:Float=12.3
+    // var num5:Double=12.3
+
+    /*
+
+
+    val readOnlyFigures = listOf("cuadrado", "triangulo", "circulo")
+    println(readOnlyFiguras)
+
+    val figura: mutableFiguras = mutableLIstOf("cuadrado", "triangulo", "circulo")
+    val readOnlyFigures = listOf("cuadrado", "triangulo", "circulo")
+    val mutableFiguras :List<String> = figura
+
+    val frutas = setOf("manzana", "banana", "naranja")
+    val frutas = mutableSetOf("manzana", "banana", "naranja")
+    */
+
+
+    val coches=mapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    println(coches)
+    val coches=mapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    println(coches2)
+
+
+
+    println("Hola MUndo!")  // NO necesita ';'
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
