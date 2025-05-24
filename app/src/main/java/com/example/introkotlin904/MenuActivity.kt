6e9df7c4e6f58cfa5823717c2a06f48c6cda0e6a
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-05-24 01:28:12 -0600

    Practica dle viernes

--- app/src/main/java/com/example/introkotlin904/MenuActivity.kt
+++ app/src/main/java/com/example/introkotlin904/MenuActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.introkotlin904.Tema4.Ejemplo4
 import tema1.Ejemplo1Activyty
 
 class MenuActivity : AppCompatActivity() {
@@ -17,11 +18,20 @@ class MenuActivity : AppCompatActivity() {
 
         //
         val btn1Ejemplo1 = findViewById<Button>(R.id.btn1)
-        val btn1Ejemplo2 = findViewById<Button>(R.id.btn2)
+        val btn2Ejemplo2 = findViewById<Button>(R.id.btn2)
+        val btn3Tarea = findViewById<Button>(R.id.btn2)
 
 
         btn1Ejemplo1.setOnClickListener {
             navegateToEjemplo1()
+        }
+
+        btn2Ejemplo2.setOnClickListener {
+            navegateToEjemplo2()
+        }
+
+        btn3Tarea.setOnClickListener {
+            navegateToTarea1()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -35,4 +45,12 @@ class MenuActivity : AppCompatActivity() {
         val intent = Intent(this, Ejemplo1Activyty::class.java)
         startActivity(intent)
     }
+
+    private fun navegateToEjemplo2() {
+        val intent = Intent(this, Ejemplo4::class.java)
+        startActivity(intent)
+    }
+
+
+
 }
--- app/src/main/java/com/example/introkotlin904/ResultadoActivity.kt
+++ app/src/main/java/com/example/introkotlin904/ResultadoActivity.kt
@@ -1,6 +1,7 @@
 package com.example.introkotlin904
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
